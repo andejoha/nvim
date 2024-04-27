c7cdb6215d1903a857c83f36ea82fea17374c5bd
Anders Johannessen <anders.johannessen@lyse.net>
2024-04-27 13:48:31 +0200

    Add nvim-lint plugin

---
+++ lua/andejoha/plugins/linter.lua
@@ -0,0 +1,23 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
+		lint.linters_by_ft = {
+			javascript = { "eslint_d " },
+			typescript = { "eslind_d" },
+			python = { "pylint" },
+		}
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = vim.api.nvim_create_augroup("lint", { clear = true }),
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+
+		vim.keymap.set("n", "<leader>l", function()
+			lint.try_lint()
+		end)
+	end,
+}
--- lua/andejoha/plugins/mason.lua
+++ lua/andejoha/plugins/mason.lua
@@ -1,33 +1,35 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-        "williamboman/mason-lspconfig.nvim",
-        "WhoIsSethDaniel/mason-tool-installer.nvim"
-    },
-    config = function()
-        require("mason").setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
-            }
-        })
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		require("mason").setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-                "tsserver"
-            }
-        })
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"tsserver",
+			},
+		})
 
-        require("mason-tool-installer").setup({
-            ensure_installed = {
-                "stylua",
-                "prettier",
-                "autopep8"
-            }
-        })
-    end
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"stylua",
+				"prettier",
+				"autopep8",
+				"eslint_d",
+				"pylint",
+			},
+		})
+	end,
 }
