980e748b33ceabf7f3e2387ba9650f56c70fea29
Anders Johannessen <anders.johannessen@lyse.net>
2024-04-27 17:00:43 +0200

    Add buffer search keymap to telescope

--- lua/andejoha/plugins/telescope.lua
+++ lua/andejoha/plugins/telescope.lua
@@ -1,25 +1,26 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function() 
-        local builtin = require("telescope.builtin")
-        local actions = require("telescope.actions")
-        
-        require("telescope").setup({
-            defaults = {
-                path_display = { "smart" },
-                mappings = {
-                    i = {
-                        ["<C-k>"] = actions.move_selection_previous,
-                        ["<C-j>"] = actions.move_selection_next
-                    }
-                }
-            }
-        })
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.6",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local builtin = require("telescope.builtin")
+		local actions = require("telescope.actions")
 
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-        vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
-        vim.keymap.set("n", "<C-f>", builtin.git_files, {})
-    end
+		require("telescope").setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous,
+						["<C-j>"] = actions.move_selection_next,
+					},
+				},
+			},
+		})
+
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+		vim.keymap.set("n", "<C-f>", builtin.git_files, {})
+	end,
 }
