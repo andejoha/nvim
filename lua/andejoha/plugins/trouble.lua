9874c61f1afc0df52f190b4f4a347092e4aef5eb
Anders Johannessen <anders.johannessen@lyse.net>
2024-08-27 12:31:44 +0200

    Fix Trouble plugin

--- lua/andejoha/plugins/trouble.lua
+++ lua/andejoha/plugins/trouble.lua
@@ -1,26 +1,40 @@
 return {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		local trouble = require("trouble")
-		local keymap = vim.keymap
-		keymap.set("n", "<leader>xx", function()
-			trouble.toggle()
-		end)
-		keymap.set("n", "<leader>xw", function()
-			trouble.toggle("workspace_diagnostics")
-		end)
-		keymap.set("n", "<leader>xd", function()
-			trouble.toggle("document_diagnostics")
-		end)
-		keymap.set("n", "<leader>xq", function()
-			trouble.toggle("quickfix")
-		end)
-		keymap.set("n", "<leader>xl", function()
-			trouble.toggle("loclist")
-		end)
-		keymap.set("n", "gR", function()
-			trouble.toggle("lsp_references")
-		end)
-	end,
+	opts = {
+		focus = true,
+	},
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
 }
