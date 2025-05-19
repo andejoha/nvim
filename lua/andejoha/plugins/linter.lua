57ecbc3f5598ce544533836f3e80ec889ca9030e
Anders Johannessen <anders.johannessen@laerdal.com>
2025-05-19 12:52:24 +0200

    Fix pylint for venv

--- lua/andejoha/plugins/linter.lua
+++ lua/andejoha/plugins/linter.lua
@@ -9,6 +9,18 @@ return {
 			python = { "pylint" },
 		}
 
+		lint.linters.pylint.cmd = "python"
+		lint.linters.pylint.args = {
+			"-m",
+			"pylint",
+			"-f",
+			"json",
+			"--from-stdin",
+			function()
+				return vim.api.nvim_buf_get_name(0)
+			end,
+		}
+
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = vim.api.nvim_create_augroup("lint", { clear = true }),
 			callback = function()
